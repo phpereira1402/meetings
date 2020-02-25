9df1d9ad4aec2c8a527a7726f38a746b13877bec
Paulo Henrique Pereira <phpereira1402@gmail.com>
2020-02-25 14:12:05 -0300

    Adicionado vuejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,9 @@ require("jquery")
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
 
+import Calendar from 'tui-calendar'; /* ES6 */
+import "tui-calendar/dist/tui-calendar.css";
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
