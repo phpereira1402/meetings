ed0855eabe9c8010ff6a4f2b86f34b89f330eef1
Paulo Henrique Pereira <phpereira1402@gmail.com>
2020-02-25 11:49:21 -0300

    Ajustes no menu

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+
 const webpack = require('webpack')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery'
-}))
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: ['jquery/src/jquery'],
+    jQuery: 'jquery/src/jquery',
+    //Chart: 'chart.js/dist/Chart',
+    //SimpleBar: 'simplebar/dist/simplebar',
+    // trying to load jquery-scroll-lock plugin
+    // scrollLock: 'jquery-scroll-lock/dist/jquery-scrollLock', <- this doesn't seem to work
+  })
+)
 
 module.exports = environment
